461ccb9b8aa6232153e6110a5a5d30c166881487
Zsolt Szilagyi <esgott@gmail.com>
2014-03-01 20:04:59 +0100

    correct multiple moves when swypeing

--- src/hu/esgott/CarMenu/leap/LeapListener.java
+++ src/hu/esgott/CarMenu/leap/LeapListener.java
@@ -2,6 +2,7 @@ package hu.esgott.CarMenu.leap;
 
 import hu.esgott.CarMenu.menu.MenuList;
 
+import com.leapmotion.leap.Config;
 import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Frame;
 import com.leapmotion.leap.Gesture;
@@ -16,6 +17,10 @@ public class LeapListener extends Listener {
 	private MenuList menuList;
 	private ExitTimer exitTimer;
 	private int previousSwipeGestureId;
+	private int frameUntilNextSwipe = 0;
+	private boolean swipeProcessed;
+	private static final int MAX_SWYPE_FREQ = 10;
+	private static final float MIN_SWIPE_LENGTH = 300.0f;
 	private static final float SPEED_LIMIT = 10.0f;
 
 	public LeapListener(MenuList menuList) {
@@ -27,11 +32,19 @@ public class LeapListener extends Listener {
 	public void onConnect(Controller controller) {
 		controller.enableGesture(Gesture.Type.TYPE_SWIPE);
 		controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+		Config config = controller.config();
+		if (config.setFloat("Gesture.Swipe.MinLength", MIN_SWIPE_LENGTH)) {
+			config.save();
+		}
 	}
 
 	@Override
 	public void onFrame(Controller controller) {
 		Frame frame = controller.frame();
+		swipeProcessed = false;
+		if (frameUntilNextSwipe > 0) {
+			frameUntilNextSwipe--;
+		}
 		GestureList gestures = frame.gestures();
 		for (Gesture gesture : gestures) {
 			handleGesture(gesture);
@@ -58,7 +71,8 @@ public class LeapListener extends Listener {
 
 	private void handleSwipeGesture(Gesture gesture) {
 		SwipeGesture swipeGesture = new SwipeGesture(gesture);
-		if (previousSwipeGestureId != swipeGesture.id()) {
+		if ((previousSwipeGestureId != swipeGesture.id()) && (!swipeProcessed)
+				&& (frameUntilNextSwipe == 0)) {
 			Vector direction = swipeGesture.direction();
 			if (direction.getX() > 0) {
 				menuList.next();
@@ -66,7 +80,9 @@ public class LeapListener extends Listener {
 				menuList.previous();
 			}
 			previousSwipeGestureId = swipeGesture.id();
+			swipeProcessed = true;
 		}
+		frameUntilNextSwipe = MAX_SWYPE_FREQ;
 	}
 
 	private void checkIfExit(Frame frame) {
